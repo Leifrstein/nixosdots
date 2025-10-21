32e58ed79a1eba2241bcf9e372393f20b8a28389
Lucas <lucaspc@pm.me>
2025-10-21 12:39:02 -0300

    .

--- home-modules/librewolf.nix
+++ home-modules/librewolf.nix
@@ -116,20 +116,6 @@
               settings = {
                 redirects = [
                   {
-                    description = "FreeTube";
-                    exampleUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-                    # Normally automatically generated, but will not be properly generated if missing
-                    # Does not cause serious problems if missing, just mangles example in redirector list
-                    exampleResult = "freetube://https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-                    includePattern = "((https://)?(www\\.)?youtu(be\\.com|\\.be)/.*)";
-                    redirectUrl = "freetube://$1";
-                    patternType = "R"; # Regular expression
-                    # Required or redirector will not work
-                    appliesTo = [
-                      "main_frame"
-                    ];
-                  }
-                  {
                     description = "Steam Client";
                     exampleUrl = "https://store.steampowered.com/";
                     # Normally automatically generated, but will not be properly generated if missing
@@ -141,18 +127,6 @@
                     appliesTo = [
                       "main_frame"
                     ];
-                  }
-                  {
-                    description = "[Farside] General Entry";
-                    exampleUrl = "https://m.youtube.com/watch?v=dQw4w9WgXcQ";
-                    # Normally automatically generated, but will not be properly generated if missing
-                    # Does not cause serious problems if missing, just mangles example in redirector list
-                    exampleResult = "https://farside.link/youtube.com/watch?v=dQw4w9WgXcQ";
-                    includePattern = "^(?:https?://)?(?:www\\.)?(?:\\w{2;}\\.)?(?:mobile\\.|m\\.)?((?:imdb|imgur|instagram|medium|odysee|quora|reddit|tiktok|translate\\.google|wikipedia|youtube)\\.(?:com|org|au|de|co|cn).*)$";
-                    redirectUrl = "https://farside.link/$1";
-                    patternType = "R"; # Regular expression
-                    # Required or redirector will not work
-                    appliesTo = ["main_frame"];
                   }
                 ];
               };
