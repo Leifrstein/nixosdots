bf45ed3e2959f6b36ff0c9b5783cc72f1381b1cd
Lucas <lucaspc@pm.me>
2025-11-04 20:31:28 -0300

    Persist proton.

--- nixos-modules/persistence.nix
+++ nixos-modules/persistence.nix
@@ -26,75 +26,76 @@
       "/etc/machine-id" # Unique system id for logging, etc.
     ];
     users.leifrstein = {
-			directories = [
-				# Default directories I care about
-				"Documents"
-				"Downloads"
-				"Books"
-				"Cloud"
-				"Games"
-				"Music"
-				"Pictures"
-				"Videos"
+      directories = [
+        # Default directories I care about
+        "Documents"
+        "Downloads"
+        "Books"
+        "Cloud"
+        "Games"
+        "Music"
+        "Pictures"
+        "Videos"
 
-				# Gaming-specific stuff
-				".cache/lutris" # Lutris banner cache
-				".config/lutris" # Lutris games and settings
-				".config/unity3d" # Needed for some games' settings
-				".config/0ad" # 0 A.D. configuration
-				".config/obs-studio" # OBS configuration
-				".local/share/lutris" # Lutris runtime data
-				".local/share/bottles" # Bottles runtime data
-				".local/share/PrismLauncher" # Prism Launcher data
-				".local/share/Steam" # Steam games and save data
-				".local/share/0ad" # 0 A.D. save data
-				".klei" # Don't Starve Together data
-				".xlcore" # Final Fantasy XIV data
+        # Gaming-specific stuff
+        ".cache/lutris" # Lutris banner cache
+        ".config/lutris" # Lutris games and settings
+        ".config/unity3d" # Needed for some games' settings
+        ".config/0ad" # 0 A.D. configuration
+        ".config/obs-studio" # OBS configuration
+        ".local/share/lutris" # Lutris runtime data
+        ".local/share/bottles" # Bottles runtime data
+        ".local/share/PrismLauncher" # Prism Launcher data
+        ".local/share/Steam" # Steam games and save data
+        ".local/share/0ad" # 0 A.D. save data
+        ".klei" # Don't Starve Together data
+        ".steam/root/compatibilitytools.d" # Proton folder
+        ".xlcore" # Final Fantasy XIV data
 
-				# Other important stuff
-				".config/calibre" # Calibre data
-				".config/ludusavi" # ludusavi configuration
-				".config/mpv" # MPV scripts and history
-				".config/qBittorrent" # qBittorrent configuration
-				".local/share/qBittorrent" # qBittorrent state
-				".config/geany" # Geany IDE configuration
-				".local/bin" # Misc binaries
-				".local/share/applications" # AppImage misc.
-				".local/share/keyrings" # Gnome Keyrings
-				".local/share/nautilus" # Nautilus state
-				".local/share/TelegramDesktop" # Telegram account data
-				".local/state/lazygit"
-				".local/state/wireplumber"
-				".mozilla" # Firefox data
-				".librewolf" # Librewolf data
-				"Sync" # Syncthing
-				".tuxpaint" # Tux Paint saves
-				# SSH key
-				{
-					directory = ".ssh";
-					mode = "u=rwx,g=,o=";
-				}
+        # Other important stuff
+        ".config/calibre" # Calibre data
+        ".config/ludusavi" # ludusavi configuration
+        ".config/mpv" # MPV scripts and history
+        ".config/qBittorrent" # qBittorrent configuration
+        ".local/share/qBittorrent" # qBittorrent state
+        ".config/geany" # Geany IDE configuration
+        ".local/bin" # Misc binaries
+        ".local/share/applications" # AppImage misc.
+        ".local/share/keyrings" # Gnome Keyrings
+        ".local/share/nautilus" # Nautilus state
+        ".local/share/TelegramDesktop" # Telegram account data
+        ".local/state/lazygit"
+        ".local/state/wireplumber"
+        ".mozilla" # Firefox data
+        ".librewolf" # Librewolf data
+        "Sync" # Syncthing
+        ".tuxpaint" # Tux Paint saves
+        # SSH key
+        {
+          directory = ".ssh";
+          mode = "u=rwx,g=,o=";
+        }
 
-				# Non-critical caches and data to persist
-				".cache/spotify" # Spotify cache
-				".cache/tealdeer" # Tldr pages, prevents tealdeer redownloading them every time
-				".config/MangoHud" # MangoHud
-				".config/GIMP" # GIMP settings
-				".config/LanguageTool" # LanguageTool settings
-				".config/libreoffice" # Libreoffice settings
-				".config/spotify" # Spotify user data
-				".config/strawberry" # Strawberry settings
-				".config/vesktop/sessionData" # Vesktop user data
-				".config/StardewValley" # Stardew Valley data
-				".local/share/strawberry" # Strawberry cache
-				".local/share/zathura" # Zathura bookmarks, etc.
-				".local/share/zoxide" # Zoxide history
-				".local/state/syncthing" # Syncthing settings
-			];
-			files = [
-				".config/gtk-3.0/bookmarks" # Nautilus bookmarks
-			];
-		};
+        # Non-critical caches and data to persist
+        ".cache/spotify" # Spotify cache
+        ".cache/tealdeer" # Tldr pages, prevents tealdeer redownloading them every time
+        ".config/MangoHud" # MangoHud
+        ".config/GIMP" # GIMP settings
+        ".config/LanguageTool" # LanguageTool settings
+        ".config/libreoffice" # Libreoffice settings
+        ".config/spotify" # Spotify user data
+        ".config/strawberry" # Strawberry settings
+        ".config/vesktop/sessionData" # Vesktop user data
+        ".config/StardewValley" # Stardew Valley data
+        ".local/share/strawberry" # Strawberry cache
+        ".local/share/zathura" # Zathura bookmarks, etc.
+        ".local/share/zoxide" # Zoxide history
+        ".local/state/syncthing" # Syncthing settings
+      ];
+      files = [
+        ".config/gtk-3.0/bookmarks" # Nautilus bookmarks
+      ];
+    };
   };
   boot.tmp.cleanOnBoot = true; # Cleans the persisted tmp on boot
   # Needed for root to access bind mounted dirs created by impermanence
