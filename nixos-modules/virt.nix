fc68a5d017db086cc15fe727993a752f8bbf8637
Lucas <lucaspc@pm.me>
2025-12-22 14:14:51 -0300

    .

--- nixos-modules/virt.nix
+++ nixos-modules/virt.nix
@@ -1,30 +1,29 @@
-{pkgs, ...}
-{
-	virtualization = {
-		libvirtd = {
-			enable = true;
-			qemu = {
-				package = pkgs.qemu_kvm;
-				vhostUserPackages = with pkgs; [virtiofsd];
-				swtpm.enable = true;
-				ovmf.enable = true;
-				ovmf.packages = [pkgs.OVMFFull.fd];
-			};
-		};
-	};
-	spiceUSBRedirection.enable = true;
-	
-	programs.virt-manager.enable = true;
-	
-	environment.systemPackages = with pkgs; [
-		dnsmasq
-		spice
-		spice-gtk
-		spice-protocol
-		virt-viewer
-		win-virtio
-		win-spice
-	];
-	
-	#users.groups.libvirtd.members = ["leifrstein"];
+{pkgs, ...}: {
+  virtualization = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        vhostUserPackages = with pkgs; [virtiofsd];
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
+    };
+  };
+  spiceUSBRedirection.enable = true;
+
+  programs.virt-manager.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    dnsmasq
+    spice
+    spice-gtk
+    spice-protocol
+    virt-viewer
+    win-virtio
+    win-spice
+  ];
+
+  #users.groups.libvirtd.members = ["leifrstein"];
 }
